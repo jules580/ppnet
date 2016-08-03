9363690190502e243d02b6cd0653c7a49ae6cd7a
BAER <vincent.baer@thalesgroup.com>
2016-08-03 17:10:55 +0200

    add test4

---
+++ test4/scenariopublicroomvector.scala
@@ -0,0 +1,15 @@
+
+package test4
+
+import scala.concurrent.duration
+
+import io.gatling.core.Predef._
+//import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import Trait._
+import Init2._
+import Login2._
+import CreatePublicRoom2._
+object AdminsPublicRoomVector{
+		val admin=scenario("Admins").exec(init2,login2,createpublicroom2)
+}
