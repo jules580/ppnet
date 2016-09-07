5151057e1ff10d9eb8f17e667f6ab9cc2680b9af
jules580 <baervincent@live.fr>
2016-09-07 10:53:47 +0000

    add last

---
+++ dockers-matrix/adds/start.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+OPTION="${1}"
+
+if [ ! -z "${ROOTPATH}" ]; then
+	echo ":: We have changed the semantic and doesn't need the ROOTPATH"
+	echo ":: variable anymore"
+fi
+
+case $OPTION in
+	"start")
+		if [ -f /data/turnserver.conf ]; then
+			echo "-=> start turn"
+			/usr/local/bin/turnserver --daemon -c /data/turnserver.conf
+		fi
+
+		echo "-=> start vector.im client"
+		(
+			if [ -f /data/vector.im.conf ]; then
+				options=""
+
+				while read -r line; do
+					[ "${line:0:1}" == "#" ] && continue
+					[ "${line:0:1}" == " " ] && continue
+					options="${options} ${line}"
+				done < /data/vector.im.conf
+
+				cd /vector-web/vector
+				echo "-=> vector.im options: http-server ${options}"
+				http-server ${options} &
+			fi
+		)
+
+		echo "-=> start matrix"
+		python -m synapse.app.homeserver \
+		       --config-path /data/homeserver.yaml \
+		;;
+	"stop")
+		echo "-=> stop matrix"
+		echo "-=> via docker stop ..."
+		;;
+	"version")
+		VERSION=$(tail -n 1 /synapse.version)
+		echo "-=> Matrix Version: ${VERSION}"
+		;;
+	"generate")
+		turnkey=$(pwgen -s 64 1)
+		echo "-=> generate turn config"
+		echo "lt-cred-mech" > /data/turnserver.conf
+		echo "use-auth-secret" >> /data/turnserver.conf
+		echo "static-auth-secret=${turnkey}" >> /data/turnserver.conf
+		echo "realm=turn.${SERVER_NAME}" >> /data/turnserver.conf
+		echo "cert=/data/${SERVER_NAME}.tls.crt" >> /data/turnserver.conf
+		echo "pkey=/data/${SERVER_NAME}.tls.key" >> /data/turnserver.conf
+
+		echo "-=> generate vector.im server config"
+		echo "# change this option to your needs" >> /data/vector.im.conf
+		echo "-p 8080" > /data/vector.im.conf
+		echo "-A 0.0.0.0" >> /data/vector.im.conf
+		echo "-c 3500" >> /data/vector.im.conf
+		echo "--ssl" >> /data/vector.im.conf
+		echo "--cert /data/${SERVER_NAME}.tls.crt" >> /data/vector.im.conf
+		echo "--key /data/${SERVER_NAME}.tls.key" >> /data/vector.im.conf
+
+		echo "-=> generate synapse config"
+		python -m synapse.app.homeserver \
+		       --config-path /data/homeserver.yaml \
+		       --generate-config \
+                       --report-stats yes \
+		       --server-name ${SERVER_NAME}
+
+		echo "-=> configure some settings in homeserver.yaml"
+		awk -v SERVER_NAME="${SERVERNAME}" \
+		    -v TURNURIES="turn_uris: [\"turn:${SERVER_NAME}:3478?transport=udp\", \"turn:${SERVER_NAME}:3478?transport=tcp\"]" \
+		    -v TURNSHAREDSECRET="turn_shared_secret: \"${turnkey}\"" \
+		    -v PIDFILE="pid_file: /data/homeserver.pid" \
+		    -v DATABASE="database: \"/data/homeserver.db\"" \
+		    -v LOGFILE="log_file: \"/data/homeserver.log\"" \
+		    -v MEDIASTORE="media_store_path: \"/data/media_store\"" \
+		    '{
+			sub(/turn_shared_secret: "YOUR_SHARED_SECRET"/, TURNSHAREDSECRET);
+			sub(/turn_uris: \[\]/, TURNURIES);
+			sub(/pid_file: \/homeserver.pid/, PIDFILE);
+			sub(/database: "\/homeserver.db"/, DATABASE);
+			sub(/log_file: "\/homeserver.log"/, LOGFILE);
+			sub(/media_store_path: "\/media_store"/, MEDIASTORE);
+			print;
+		    }' /data/homeserver.yaml > /data/homeserver.tmp
+		mv /data/homeserver.tmp /data/homeserver.yaml
+
+		echo "-=> you have to review the generated configuration file homeserver.yaml"
+		;;
+	*)
+		echo "-=> unknown \'$OPTION\'"
+		;;
+esac
---
+++ dockers-matrix/readme.md
@@ -0,0 +1,101 @@
+
+# Introduction
+
+Dockerfile for installation of [matrix] open federated Instant Messaging and
+VoIP communication server.
+
+The vector web client has now his own docker file at [github].
+
+[matrix]: matrix.org
+[github]: https://github.com/silvio/docker-matrix-vector
+
+# Configuration
+
+To configure run the image with "generate" as argument. You have to setup the
+server domain and a `/data`-directory. After this you have to edit the
+generated homeserver.yaml file.
+
+To get the things done, "generate" will create a own self-signed certificate.
+
+> This needs to be changed for production usage.
+
+Example:
+
+    $ docker run -v /tmp/data:/data --rm -e SERVER_NAME=localhost silviof/docker-matrix generate
+
+# Start
+
+For starting you need the port bindings and a mapping for the
+`/data`-directory.
+
+    $ docker run -d -p 8448:8448 -p 3478:3478 -v /tmp/data:/data silviof/docker-matrix start
+
+# Port configurations
+
+This following ports are used in the container. You can use `-p`-option on
+`docker run` to configure this part (eg.: `-p 443:8448`).
+
+* turnserver: 3478,3479,5349,5350 udp and tcp
+* homeserver: 8008,8448 tcp
+
+# Version information
+
+To get the installed synapse version you can run the image with `version` as
+argument or look at the container via cat.
+
+    $ docker run -ti --rm silviof/docker-matrix version
+    -=> Matrix Version
+    synapse: master (7e0a1683e639c18bd973f825b91c908966179c15)
+    coturn:  master (88bd6268d8f4cdfdfaffe4f5029d489564270dd6)
+
+    # docker exec -it CONTAINERID cat /synapse.version
+    synapse: master (7e0a1683e639c18bd973f825b91c908966179c15)
+    coturn:  master (88bd6268d8f4cdfdfaffe4f5029d489564270dd6)
+
+
+# Environment variables
+
+* `SERVER_NAME`: Server and domain name, mandatory, needed only  for `generate`
+* `REPORT_STATS`: statistic report, mandatory, values: `yes` or `no`, needed
+  only for `generate`
+
+# build specific arguments
+
+* `BV_SYN`: synapse version, optional, defaults to `master`
+* `BV_TUR`: coturn turnserver version, optional, defaults to `master`
+
+For building of synapse version v0.11.0-rc2 and coturn with commit a9fc47e add
+`--build-arg BV_SYN=v0.11.0-rc2 --build-arg BV_TUR=a9fc47efd77` to the `docker
+build` command.
+
+# diff between system and fresh generated config file
+
+To get a hint about new options etc you can do a diff between your configured
+homeserver.yaml and a newly created config file. Call your image with `diff` as
+argument.
+
+
+```
+$ docker run --rm -ti -v /tmp/data:/data silviof/docker-matrix diff
+[...]
++# ldap_config:
++#   enabled: true
++#   server: "ldap://localhost"
++#   port: 389
++#   tls: false
++#   search_base: "ou=Users,dc=example,dc=com"
++#   search_property: "cn"
++#   email_property: "email"
++#   full_name_property: "givenName"
+[...]
+```
+
+For generating of this output its `diff` from `busybox` used. The used diff
+parameters can be changed through `DIFFPARAMS` environment variable. The
+default is `Naur`.
+
+
+# Exported volumes
+
+* `/data`: data-container
+
