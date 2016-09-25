ddbc47c9fc05198a8c50a1c28d8b815deccb1695
root <root@instance-1.c.stage-vbaer.internal>
2016-09-25 13:05:10 +0000

    add userfile with picture

---
+++ user-files-picture/simulations/SendMessage.scala
@@ -0,0 +1,35 @@
+package test4
+ 
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import Trait._
+object SendMessage2 extends Trait {
+
+			val feeder=csv("answer.csv").random
+			val sendmessage2=exec(feed(feeder))
+			.exec(http("Wait Message")
+			.options("/_matrix/client/r0/rooms/${table}%3Amatrix.allmende.io/send/m.room.message/m1468424657926.0?access_token=${token}")
+			.headers(headers_13)
+			.resources(http("Message Send ")
+			.put("/_matrix/client/r0/rooms/${table}%3Amatrix.allmende.io/send/m.room.message/m1468424657926.0?access_token=${token}")
+			.headers(headers_16)
+			.body(StringBody("""{"msgtype":"m.text","body":"${answer}"}"""))
+			.check(status.is(200))
+			.check(jsonPath("$.event_id").saveAs("Event_Id")),
+            http("End Typing")
+			.put("/_matrix/client/r0/rooms/${table}%3Amatrix.allmende.io/typing/%40${login}%3Amatrix.allmende.io?access_token=${token}")
+			.headers(headers_16)
+			.body(RawFileBody("RecordedSimulateSendMessageVector_0068_request.txt"))
+			.check(status.is(200)),
+            http("Event message")
+			.get("/_matrix/client/r0/sync?filter=0&timeout=30000&since=s1548_1239_766_7_1_2&access_token=${token}")
+			.headers(headers_14)
+			.check(status.is(200)))
+			.check(status.is(200)))
+			.exec(session =>{
+				println("session VB"+session)
+				session
+			})
+		}
---
+++ user-files-picture/simulations/scenariologinmatrix.scala
@@ -0,0 +1,11 @@
+  package test5
+  import scala.concurrent.duration._
+  import io.gatling.core.Predef._
+  import io.gatling.http.Predef._
+  import io.gatling.jdbc.Predef._
+  import Init_Matrix2._
+  import Login_Matrix2._
+  import Trait_Matrix._
+  object AdminsLoginMatrix{
+	  val admin=scenario("Admin").exec(init_matrix2,login_matrix2)
+  }
