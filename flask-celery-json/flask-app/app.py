a21bba2133f662418bbd6ae0646cdb11ed3df61e
jules580 <baervincent@live.fr>
2016-09-27 13:12:33 +0000

    app.py

--- flask-celery-json/flask-app/app.py
+++ flask-celery-json/flask-app/app.py
@@ -74,56 +74,16 @@ app.config['SWAGGER'] = {
 }
 
 swagger = Swagger(app)
-@app.route('/add/<int:param1>/<int:param2>')
-def add(param1,param2):
-    task = celery.send_task('mytasks.add', args=[param1, param2], kwargs={})
-    return "<a href='{url}'>check status of {id} </a>".format(id=task.id,
-                url=url_for('check_task',id=task.id,_external=True))
 
 @app.route('/init/')
 def init():
 	session['iden_matrix_num']=0
-	#idenmatrix={}
-	#g.idenmatrix=idenmatrix
 	return str(session['iden_matrix_num'])	
-@app.route('/check/<string:id>')
-def check_task(id):
-    res = celery.AsyncResult(id)
-    if res.state==states.PENDING:
-        return res.state
-    else:
-        return str(res.result)
         
-@app.route('/requestResult/vector/<int:param4>')
-def getresults2(param4):
-	global iden
-	global iden_num
-	global listTest
-	global DataVector
-	global k
-	DataReceive=DataVector[param4]
-	testSourceName=str(param4)+"testSourceName"
-	scenarioName=str(param4)+"scenarioName"
-	
-	SourceName=DataReceive[testSourceName]
-	Scenario=DataReceive[scenarioName]
-	#del DataVector[param4]
-	#DataVector.insert(param4,"")
-
-	
-	task= celery.send_task('mytask.get', args=[Scenario,SourceName], kwargs={})
-	res= celery.AsyncResult(task.id)
-	iden_num+=1
-	iden[iden_num]=task.id
-	
-	Task={"request_id":str(iden_num)}
-	return json.dumps(Task,sort_keys=True,indent=4)
 @app.route('/requestResult/<int:param2>')
 def getresults3(param2):	
 	global idenmatrix
-	#global iden_matrix_num
 	global listTest
-	#global Data
 	Data=session['DATA']
 
 	DataReceive=Data[param2]
@@ -131,8 +91,6 @@ def getresults3(param2):
 	Scenario=DataReceive[scenarioName]
 	testSourceName=str(param2)+"testSourceName"
 	SourceName=DataReceive[testSourceName]
-	#del Data[param2]
-	#Data.insert(param2,"")
 	with open('data.json') as json_file:
 		listTest2=json.load(json_file)
 	Name=[]
@@ -148,107 +106,22 @@ def getresults3(param2):
 		SourceNames=DataR[testSourceNames]
 		scenarioNames=str(K)+"scenarioName"
 		Scenarios=DataR[scenarioNames]
-	#	if SourceNames==Name[1] and Scenarios==Scenario:
-	#		param1+=1
-	#	elif SourceNames==Name[0] and Scenarios==Scenario:
-	#		param3+=1
-	#if SourceName==Name[1]:
-	#	param2=param1
-	#elif SourceName==Name[0]:
-		#param2=param3
-		#if Scenarios==Scenario:
 		if Scenarios==Scenario:	
 			number(SourceNames)
 	param2=getresult(SourceName)
 	initialisation()		
-	#time.sleep(30)
 	task= celery.send_task('mytask.get', args=[Scenario,SourceName,param2], kwargs={})
-	#iden_matrix_num=session['iden_matrix_num']
 	iden_matrix_num=session['iden_matrix_num']
 	iden_matrix_num+=1
-	#idenmatrix=session['idenmatrix']
-	#idenmatrix=getattr(g,'idenmatrix', None)
-
-	#task= celery.send_task('mytask.get', args=[Scenario,SourceName,param2],kwargs={})
-
 	idenmatrix[iden_matrix_num]=task.id
 	session['iden_matrix_num']=session['iden_matrix_num']+1
-	#session['iden_matrix_num']=iden_matrix_num
+
 	Task={"request_id":str(iden_matrix_num),"param":param2}
 	return json.dumps(Task,sort_keys=True,indent=4)	
-@app.route('/requestResult/matrix/<int:param2>')
-def getresultsmatrix2(param2):
-	global idenmatrix
-	global iden_matrix_num
-	global listTest
-	global Data
 
-	DataReceive=Data[param2]
-	scenarioName=str(param2)+"scenarioName"
-	Scenario=DataReceive[scenarioName]
-	testSourceName=str(param2)+"testSourceName"
-	SourceName=DataReceive[testSourceName]
-	#del Data[param2]
-	#Data.insert(param2,"")
-			
-	
-	#time.sleep(30)
-	task= celery.send_task('mytask.get', args=[Scenario,SourceName], kwargs={})
-	iden_matrix_num+=1
-	idenmatrix[iden_matrix_num]=task.id
-	
-	Task={"request_id":str(iden_matrix_num)}
-	return json.dumps(Task,sort_keys=True,indent=4)
-    
-@app.route('/getresult/vector/<int:param3>')
-def check(param3):
-	global iden
-	#global iden_num
-	identi=iden[param3]
-
-	datares="Pending"
-	res= celery.AsyncResult(identi)
-	if res.state==states.PENDING:
-		Task={"status":res.state}
-		return json.dumps(Task,sort_keys=True,indent=4)	
-	else:
-		zip_file_url=res.result
-		r = requests.get(zip_file_url)
-		z = zipfile.ZipFile(io.BytesIO(r.content))
-		z.extractall()
-	        Nametab=zip_file_url.split('s/')
-		Name=Nametab[1]
-		subprocess.call(["sh","/flask-app/"+Name+"/jsonresult.sh","/flask-app/"+Name])
-		subprocess.call(["cp","-f","/flask-app/result.txt","/flask-app/"+Name+"/result.txt"])
-		time.sleep(2)
-		subprocess.call(["zip","-r",Name+"/test.zip",Name ])
-        	return send_file(Name+'/test.zip')
-
-	#del iden[param3]
-	#iden.insert(param3,"")
-@app.route('/jsonresult/vector/<int:param3>')
-def check3(param3):
-	global iden
-	global iden_num
-	identi=iden[param3]
-	res=celery.AsyncResult(identi)
-	if res.state==states.PENDING:
-		Task={"status":res.state}
-		return json.dumps(Task,sort_keys=True,indent=4)
-	else:
-		zip_file_url=res.result
-		r=requests.get(zip_file_url)
-		z = zipfile.ZipFile(io.BytesIO(r.content))
-		z.extractall()
-		Nametab= zip_file_url.split('s/')
-		Name=Nametab[1]
-		p1=subprocess.call(["sh","/flask-app/"+Name+"/jsonresult.sh","/flask-app/"+Name])
-		file= open('/flask-app/result.txt','r')
-		return file.read()
 @app.route('/getresult/<int:param3>')
 def check46(param3):
 	global idenmatrix
-	#global iden_matrix_num
 	identi=idenmatrix[param3]
    
 	res= celery.AsyncResult(identi)
@@ -270,7 +143,6 @@ def check46(param3):
 @app.route('/getresult/matrix/<int:param3>')
 def check2(param3):
 	global idenmatrix
-	#global iden_matrix_num
 	identi=idenmatrix[param3]
    
 	res= celery.AsyncResult(identi)
@@ -290,12 +162,9 @@ def check2(param3):
 		subprocess.call(["zip","-r",Name+"/test.zip",Name ])
                 return send_file(Name+'/test.zip')
 
-	#del idenmatrix[param3]
-	#idenmatrix.insert(param3,"")	
 @app.route('/jsonresults/<int:param3>')
 def check654(param3):
 	global indenmatrix
-	#idenmatrix=getattr(g,'idenmatrix', None)
 	identi=idenmatrix[param3]
 	res= celery.AsyncResult(identi)
 	if res.state==states.PENDING:
@@ -310,25 +179,7 @@ def check654(param3):
 		Name=Nametab[1]
 		subprocess.call(["sh","/flask-app/"+Name+"/jsonresult.sh","/flask-app/"+Name])
 		file= open('/flask-app/result.txt','r')
-		return file.read()  		
-@app.route('/jsonresult/matrix/<int:param3>')
-def check5(param3):
-	global indentmatrix
-	identi=idenmatrix[param3]
-	res= celery.AsyncResult(identi)
-	if res.state==states.PENDING:
-		Task={"status":res.state}
-		return json.dumps(Task,sort_keys=True,indent=4)
-	else:
-		zip_file_url=res.result
-		r = requests.get(zip_file_url)
-		z= zipfile.ZipFile(io.BytesIO(r.content))
-		z.extractall()
-		Nametab=zip_file_url.split('s/')
-		Name=Nametab[1]
-		subprocess.call(["sh","/flask-app/"+Name+"/jsonresult.sh","/flask-app/"+Name])
-		file= open('/flask-app/result.txt','r')
-		return file.read()  
+		return file.read()  		  
 @app.route("/launchtests/", methods=["PUT","POST"])
 def echo23():
 	JsonData=json.loads(json.dumps(request.get_json(force=True)))
@@ -379,120 +230,7 @@ def echo23():
 		Message=js["message"]
 		Task={"status":Status,"details":Message}
 		return json.dumps(Task,sort_keys=True,indent=4)
-@app.route("/launchtest/matrix", methods=["PUT","POST"])
-def echo():
-	JsonData=json.loads(json.dumps(request.get_json(force=True)))
-	TargetUrl=JsonData['testTargetUrl']
-	scenarioName=JsonData['scenarioName']
-	testSourceName=JsonData['testSourceName'] 
-	content=[]
-	content.append(TargetUrl)
-	content.append(scenarioName)
-	content.append(testSourceName)
-	global i
-	global Data
-	global listTest
-	i+=1
-   # content.append(str(i))
-	testTargetUrl= str(i)+"testTargetUrl"
-	scenarioName= str(i)+"scenarioName"
-	testSourceName= str(i)+"testSourceName"
-     #test= { name : content[0], name2: content[1], name3: content[2]}
-	testnum= "test"+str(i)
-	testnum={ testTargetUrl: content[0], scenarioName: content[1], testSourceName: content[2]}
-	index=i
-	Data.append(testnum)
-	Receive_data=Data[index]
-	URL=Receive_data[testTargetUrl]
-    #r=requests.get('http://192.168.2.77:')
-	r=requests.get(listTest[content[2]]+"/gatling/"+content[1]+"/start")     
-	js=json.loads(r.content)
-	Status=js["status"]
-	if Status!="failed":
-		Details=js["details"]	
-		Task={"request_id":str(i),"status":str(Status),"details":str(Details)}
-		return json.dumps(Task,sort_keys=True,indent=4) 
-	else:
-		Message=js["message"]
-		Task={"status":Status,"details":Message}
-		return json.dumps(Task,sort_keys=True, indent=4)
-	#+str(i)+" "+str(r.content)  
-    #
-    #return str(r.content)
 
-	    
-@app.route("/launchtest/vector", methods=["PUT","POST"])
-def echo2(): 
-	JsonData=json.loads(json.dumps(request.get_json(force=True)))
-	TargetUrl=JsonData['testTargetUrl']
-	scenarioName=JsonData['scenarioName']
-	testSourceName=JsonData['testSourceName']
-	TargetVectorUrl=JsonData['testTargetVectorUrl']
-	content=[]
-	content.append(TargetUrl)
-	content.append(scenarioName)
-	content.append(testSourceName)
-	content.append(TargetVectorUrl)
-	
-	global k
-	global DataVector
-	global listTest
-	k+=1
-   # content.append(str(i))
-	testTargetUrl= str(k)+"testTargetUrl"
-	scenarioName= str(k)+"scenarioName"
-	testSourceName= str(k)+"testSourceName"
-	testTargetVectorUrl=str(k)+"testTargetVectorUrl"
-     #test= { name : content[0], name2: content[1], name3: content[2]}
-
-	testnumvector={ testTargetUrl: content[0], scenarioName: content[1], testSourceName: content[2], testTargetVectorUrl: content[3]}
-	index=k
-	DataVector.append(testnumvector)
-	Receive_data_vector=DataVector[index]
-	URL=Receive_data_vector[testTargetUrl]
-	r=requests.get(listTest[content[2]]+"/gatling/"+content[1]+"/start")
-	js=json.loads((r.content))
-	Status=js['status']
-	if Status!="failed":
-		Details=js['details']
-
-		Task={"request_id":str(k),"status":str(Status),"details":str(Details)}
-		return json.dumps(Task,sort_keys=True, indent=4)
-   	else:
-		Message=js['message']
-		Task={"status":Status,"details":Message}
-		return json.dumps(Task,sort_keys=True, indent=4)
-@app.route('/test/conf/matrix/<testerId>')
-def run(testerId):
- 
-  global Data
-  #global tester
-  global i
-  global Send
-  global testreceive
-  for j in range(i,0,-1):	
-	Receive_data=Data[j]
- 	testTargetUrl=str(j)+"testTargetUrl"
- 	scenarioName=str(j)+"scenarioName"
-	testSourceName=str(j)+"testSourceName" 
-   	Tester=Receive_data[testSourceName]
-   	
-         #if tester==testerId:
- 
- 	 #   k+=1
-  	if testerId==Tester and Send==False:
- 		
-		Send=True
-   		testnum=Receive_data
- 		testTargetUrl=Receive_data[testTargetUrl]
-   		Scenario=Receive_data[scenarioName]
- 		testreceive={"url": testTargetUrl, "scenarioName": Scenario, "testSourceName": Tester}
- 		#del Data[j]
-		#Data.insert(j,"")
-                
-  JsonData=json.dumps(testreceive,sort_keys=True, indent=4)
-  Send=False
-  return JsonData
 @app.route('/test/confs/<testerId>')
 def run4(testerId):
 	global Data
@@ -524,41 +262,8 @@ def run4(testerId):
 	
 	Send=False
 	return Infos
-@app.route('/test/conf/vector/<testerId>')
-def runvector(testerId):
- 
-  global DataVector
-  #global tester
-  global k
-  global SendVector
-  global testreceivevector
-  for j in range(k,0,-1):	
-	Receive_data_vector=DataVector[j]
- 	testSourceName=str(j)+"testSourceName"
- 	scenarioName=str(j)+"scenarioName"
- 	testTargetUrl=str(j)+"testTargetUrl" 
-	testTargetVectorUrl=str(j)+"testTargetVectorUrl"
-   	Tester=Receive_data_vector[testSourceName]
-         #if tester==testerId:
- 
- 	 #   k+=1
-  	if testerId==Tester and SendVector==False:
-		SendVector=True
-   		testnum=Receive_data_vector
- 		Matrix=Receive_data_vector[testTargetUrl]
-   		Vector=Receive_data_vector[testTargetVectorUrl]
- 		testreceivevector=Matrix+","+Vector
- 		#del DataVector[j]
- 		#k-=1
-                
-  
-  SendVector=False
-  return testreceivevector
 @app.route('/checkstatus/<int:id>')
 def check54(id):
-	#global listTest
-	#global Data
-	#global i
 	Data=session['DATA']
 	i=session['index']
 	DataReceive=Data[id]
@@ -570,48 +275,12 @@ def check54(id):
 		listTest2=json.load(json_file)
         urlsend=listTest2[test]+"/gatling/"+Scenario
         r=requests.get(urlsend)
-        #return "hello"+Scenario
-	js= json.loads((r.content))
-	Task={"scenarioName":Scenario,"status":js['status']}
-	return json.dumps(Task,sort_keys=True,indent=4)
-@app.route('/checkstatus/matrix/<int:id>')
-def checks(id):
-	global listTest
-	global DataVector
-	global i
-	DataReceive=Data[id]
-	testSourceName=str(id)+"testSourceName"
-	test=DataReceive[testSourceName]
-	scenarioName=str(id)+"scenarioName"
-	Scenario=DataReceive[scenarioName]
-        urlsend=listTest[test]+"/gatling/"+Scenario
-        r=requests.get(urlsend)
-        #return "hello"+Scenario
 	js= json.loads((r.content))
 	Task={"scenarioName":Scenario,"status":js['status']}
 	return json.dumps(Task,sort_keys=True,indent=4)
 
-@app.route('/checkstatus/vector/<int:id>')
-def checks2(id):
-        global listTest
-        global DataVector
-        global k
-        DataReceive=DataVector[id]
-        testSourceName=str(id)+"testSourceName"
-        test=DataReceive[testSourceName]
-        scenarioName=str(id)+"scenarioName"
-	Scenario=DataReceive[scenarioName]
-        urlsend=listTest[test]+"/gatling/"+Scenario
-        r=requests.get(urlsend)
-        #return "hello"+Scenario
-        js= json.loads((r.content))
-        Task={"scenarioName":Scenario,"status":js['status']}
-	return json.dumps(Task,sort_keys=True,indent=4)
 @app.route('/getreports/<int:id>')
 def getreports4(id):
-	#global listTest
-	#global Data
-	#global i
 	Data=session['DATA']
 	i=session['index']
 	DataReceive=Data[id]
@@ -637,65 +306,6 @@ def getreports4(id):
 		tab+= "No simulation"
 	Task={"Tab":[tab]}
 	return json.dumps(Task,sort_keys=True,indent=4)
-@app.route('/getreports/matrix/<int:id>')
-def getreports(id):
-	global listTest
-        global Data
-	global i
-	DataReceive=Data[id]
-	testSourceName=str(id)+"testSourceName"
-	test=DataReceive[testSourceName]
-	scenarioName=str(id)+"scenarioName"
-	ScenarioData=DataReceive[scenarioName]
-	Scenariotab=ScenarioData.split('.')
-	Scenario=Scenariotab[2]
-        urlresult=listTest[test]+"/gatling/"+Scenario+"/reports"
-	r=requests.get(urlresult)
-	js=json.loads((r.content))
-	tabjs=js['reports']
-	index=len(tabjs)-1
-        tab=" "
-	if index>=0:
-		for p in range(0,index-1):
-			tab+=tabjs[p]+", "
-		tab+=tabjs[index]
-	else:
-		tab+= "No simulation"
-	Task={"Tab":[tab]}
-	return json.dumps(Task,sort_keys=True,indent=4)
-@app.route('/getreports/vector/<int:id>')
-def getreports2(id):
-        global listTest
-        global DataVector
-        global k
-        DataReceive=DataVector[id]
-        testSourceName=str(id)+"testSourceName"
-        test=DataReceive[testSourceName]
-	scenarioName=str(id)+"scenarioName"
-	ScenarioData=DataReceive[scenarioName]
-	ScenarioTab=ScenarioData.split('.')
-	Scenario=ScenarioTab[2]
-        urlresult=listTest[test]+"/gatling/"+Scenario+"/reports"
-        r=requests.get(urlresult)
-        js=json.loads((r.content))
-        tabjs=js['reports']
-        index=len(tabjs)-1
-	tab=" "
-	if index>=0:
-		for l in range(0,index-1):
-			tab+=tabjs[l]+","
-       		tab+=tabjs[index]
-	else:
-		tab+=" No simulation"
-	Task={"Tab":[str(tab)]}
-        return json.dumps(Task,sort_keys=True, indent=4)
-@app.route('/test', methods=["PUT","POST"])
-def new():
-	JsonData=json.loads(json.dumps(request.get_json(force=True)))
-	url=JsonData['testTargetUrl']
-	scenarioName=JsonData['scenarioName']
-	testSourceName=JsonData['testSourceName']
-	return "Mon url est:"+url+", Mon scenario est:"+scenarioName+",Mon testeur est:"+testSourceName
 app.secret_key="test"	
 class VectorAPI(MethodView):
     def post(self):
